a1dd16eea8600ea64446d2781b83331370173934
Parusakovich <Parusakovich@gmail.com>
2022-02-21 23:56:49 +0300

    Добавьте файлы проекта.

---
+++ MySecondSolution/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+namespace FirstConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение радиуса конуса");
+            double Radius = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Введите значение высоты конуса");
+            double Height = int.Parse(Console.ReadLine());
+
+            const double PI = Math.PI;
+            double Square = PI * Radius * (Radius + Height);
+
+            Console.WriteLine("Площадь конуса: " + Square);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ SecondConsole/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+namespace SecondConsole
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 14;
+            int y = 1;
+            int z = 5;
+
+            {
+                x += y - x++ * z;
+                Console.WriteLine("Результат выражения x += y - x++ * z: " + x);
+            }
+
+            {
+                z = --x - y * 5;
+                Console.WriteLine("Результат выражения z = --x – y * 5: " + z);
+            }
+
+            {
+                y /= x + 5 % z;
+                Console.WriteLine("Результат выражения y /= x + 5 % z: " + y);
+            }
+
+            {
+                z = x++ + y * 5;
+                Console.WriteLine("Результат выражения z = x++ + y * 5: " + z);
+            }
+
+            {
+                x = y - x++ * z;
+                Console.WriteLine("Результат выражения x = y - x++ * z: " + x);
+            }
+
+        }
+    }
+}
